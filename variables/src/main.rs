3bb49fb553696e6ed102ed5019d9bd8aafbaa29e
walkeranalyticsllc <dan@walkeranalyticsllc.com>
2023-07-30 09:31:10 -0400

    Working through shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
+    // step 0 
     let x = 5;
-
+    // step 1
     let x = x + 1;
-
-    {
+    // step 1 is shadowed by what's inside here
+    {   // step 2
         let x = x * 2;
         println!("The value of x in the inner scope is: {x}");
     }
-
+    // back to step 1 val
     println!("The value of x is: {x}");
 }
